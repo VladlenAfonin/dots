767af8c859e3fed788f16154d72e8dea59ec0e63
vladlenafonin <vladlenafonin.university@yandex.ru>
2025-03-17 23:45:10 +0300

    chore: update init.lua

--- init.lua
+++ init.lua
@@ -52,6 +52,8 @@ lazy.setup({
     { "saadparwaiz1/cmp_luasnip" },
     { "sbdchd/neoformat" },
     { "https://codeberg.org/FelipeLema/cmp-async-path" },
+    { "ctrlpvim/ctrlp.vim" },
+    { "hedyhli/markdown-toc.nvim" },
 })
 
 require("onedark").load()
@@ -77,6 +79,80 @@ require("nvim-treesitter.configs").setup({
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+    },
+})
+require("mtoc").setup({
+    -- Config relating to fetching of headings to be included in ToC
+    headings = {
+        -- Include headings before the ToC (or current line for `:Mtoc insert`)
+        before_toc = false,
+        -- Either list of lua patterns (regex),
+        -- or a function that takes a heading title and returns boolean (true means
+        -- to EXCLUDE heading).
+        exclude = {},
+        -- The first capture is for heading level ('###') and second is for the heading
+        -- title.
+        pattern = "^(#+)%s+(.+)$",
+    },
+
+    -- Config relating to the style and format of the ToC
+    toc_list = {
+        -- string or list of strings (for cycling)
+        -- If cycle_markers = false and markers is a list, only the first is used.
+        -- You can set to '1.' to use a automatically numbered list for ToC (if
+        -- your markdown render supports it).
+        markers = "-",
+        cycle_markers = false,
+        -- Example config for cycling markers:
+        ----- markers = {'*', '+', '-'},
+        ----- cycle_markers = true,
+
+        -- Integer or a function that returns an integer.
+        -- If function, it is called every time the ToC is regenerated. This allows the use
+        -- of retrieving buffer-local settings like shiftwidth.
+        indent_size = 4,
+
+        -- Remove the ${indent} below, or set indent_size=0 to have the whole ToC
+        -- be a flattened list.
+        item_format_string = "${indent}${marker} [${name}](#${link})",
+
+        ---Formatter for a single ToC list item.
+        -- `item_info` has fields `name`, `link`, `marker`, `indent`, To change the
+        -- format of each heading item but keep the same field substitution syntax,
+        -- simply change `item_format_string`.
+        ---@param item_info table Information for current heading item.
+        ---@param fmtstr string from `item_format_string` config
+        ---@return string formatted_item
+        item_formatter = function(item_info, fmtstr)
+            local s = fmtstr:gsub([[${(%w-)}]], function(key)
+                return item_info[key] or ("${" .. key .. "}")
+            end)
+            return s
+        end,
+    },
+
+    -- Table or boolean. Set to true to use these defaults, set to false to disable completely.
+    -- Fences are needed for the update/remove commands.
+    fences = {
+        enabled = true,
+        -- These fence texts are wrapped within "<!-- % -->", where the '%' is
+        -- substituted with the text.
+        start_text = "mtoc start",
+        end_text = "mtoc end",
+        -- An empty line is inserted on top and below the ToC list before the being
+        -- wrapped with the fence texts, same as vim-markdown-toc.
+    },
+
+    -- Set auto_update=true to use the following defaults.
+    -- Set to false to disable completely.
+    -- Fields events and pattern are used unprocessed for creating autocmds.
+    auto_update = {
+        enabled = true,
+        -- This allows the ToC to be refreshed silently on save for any markdown file.
+        -- The refresh operation uses `Mtoc update` and does NOT create the ToC if
+        -- it does not exist.
+        events = { "BufWritePre" },
+        pattern = "*.{md,mdown,mkd,mkdn,markdown,mdwn}",
     },
 })
 
@@ -186,7 +262,19 @@ lspconfig_defaults.capabilities =
     vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.c", "*.h", "*.zig", "*.lua", "*.rust", "*.py", "*.sh", "*.zshrc", "*.cmake", "CMakeLists.txt" },
+    pattern = {
+        "*.c",
+        "*.h",
+        "*.zig",
+        "*.lua",
+        "*.rust",
+        "*.py",
+        "*.sh",
+        "*.zshrc",
+        "*.cmake",
+        "CMakeLists.txt",
+        "*.hs",
+    },
     desc = "Neoformat",
     command = "Neoformat",
 })
@@ -249,6 +337,9 @@ require("lspconfig").rust_analyzer.setup({})
 require("lspconfig").clangd.setup({})
 require("lspconfig").pyright.setup({})
 require("lspconfig").bashls.setup({})
+require("lspconfig").hls.setup({
+    filetypes = { "haskell", "lhaskell", "cabal" },
+})
 require("lspconfig").lua_ls.setup({
     on_init = function(client)
         if client.workspace_folders then
